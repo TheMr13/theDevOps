944588ec0da0607f74223c747eb1230ba9444043
Daniel <daniel@mcguinness.net.nz>
2017-12-07 16:17:33 +1300

    some stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ gulp.task('default', ['start_server', 'connect']);
 
 gulp.task('start_server', function(cb) {
   //some code
+  cb();
 });
 
 gulp.task('connect', ['start_server'], function(cb) {
   //some code
+  cb();
 });
