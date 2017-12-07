b95cb2d847935e221269a6c04e7ebdea4c1fe057
Daniel <daniel@mcguinness.net.nz>
2017-12-07 16:34:15 +1300

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 const gulp = require('gulp');
+const http = require('http');
+const connect = require('connect');
+const serveStatic = require('serve-static');
+const exec = require('child_process').exec;
 
 gulp.task('default', ['start_server', 'connect']);
 
